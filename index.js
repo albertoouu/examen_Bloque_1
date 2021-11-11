260ff28736124748adc6ef8ea0efdb6da31279fb
albertoouu <albertoouu@gmail.com>
2021-11-11 11:41:09 -0600

    add coments

--- index.js
+++ index.js
@@ -4,17 +4,20 @@ fetch("./alumnos.json")
   .catch((error) => console.log(error));
 
 let bootcampData = (data) => {
-  console.log(data);
-  pintaDatos(data.integrantes);
+  //console.log(data);
+  for (int of data.integrantes) {
+    //---- Muestra en consola los integrantes del bootcamp---------
+    console.log(
+      `Nombre: ${int.nombre} Apellido: ${int.apellido} Edad: ${int.edad} Cumpleaños: ${int.cumpleaños}`
+    );
+  } //-------------------------------------------------------------------------------------------
+  pintaDatos(data.integrantes); // Llama a la función independiente solo para pintar los datos en el HTML
 };
 
 let pintaDatos = (datos) => {
   let bootcampMembers = document.getElementById("BootcampMembers");
   let cumpleaños = document.getElementById("cumpleaños");
   for (int of datos) {
-    console.log(
-      `Nombre: ${int.nombre} Apellido: ${int.apellido} Edad: ${int.edad} Cumpleaños: ${int.cumpleaños}`
-    );
     bootcampMembers.innerHTML += `Nombre: ${int.nombre}.  Apellido: ${int.apellido}.  Edad: ${int.edad}.  Cumpleaños: ${int.cumpleaños}.<br>`;
     cumpleaños.innerHTML += `${int.nombre} cumpleaños: ${int.cumpleaños}<br>`;
   }
